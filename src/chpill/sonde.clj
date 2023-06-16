1acfeac02ff728a29af5302e1c17a8487cbc5d9c
chpill <chpill@example.com>
2023-06-16 18:43:43 +0200

    Re-implement the macro and properly document it

--- src/chpill/sonde.clj
+++ src/chpill/sonde.clj
@@ -1,44 +1,21 @@
-(ns chpill.sonde
-  (:require [sc.api]
-            [sc.impl :as i]))
+(ns chpill.sonde)
 
 
-;; Heinous ripoff of `sc.api/defsc` that lets you define the var in any namepace
-(defmacro internsc
-  [ns ep-id]
-  (let [cs (i/resolve-code-site ep-id)
-        ep-id (i/resolve-ep-id ep-id)]
-    (into []
-      (map (fn [ln]
-             `(intern ~ns (quote ~ln) (i/ep-binding ~ep-id (quote ~ln)))))
-      (:sc.cs/local-names cs))))
-
-
-;; TODO @chpill if we are only interested in the latest value for the context,
-;; we could and should get rid of `eval`. To do this, we must understand how
-;; scope-capture emits code and reuse/reproduce parts of it.
 (defmacro spy
-  "VERY experimental macro, use at your own risk.
-
-  Uses the scope-capture library to register context at the call-site, and
-  generate bindings in place at runtime. Because of the `eval` inside, we must
-  be careful to define the vars in the namespace of the call site. Otherwise,
-  the vars are defined in `clojure.core`, which means, in addition to being all
-  around nasty, that they won't be properly cleaned up by tools.namespace during
-  code reloads."
+  "Emits a `def` for each local binding at the call site, then evaluates the given form."
   [form]
-  (let [call-site-ns *ns*]
-    `(let [x# (sc.api/spyqt ~form)]
-       (eval (list 'chpill.sonde/internsc ~call-site-ns (sc.api/last-ep-id)))
-       x#)))
-
-(def nrepl-middleware
-  "To be injected with the rest of the tooling ahead of code you'd like to
-  probe. This allows the `chpill.sonde/spy` macro to be called from anywhere
-  without having to require it inside your code."
-  identity)
+  `(do ~@(map (fn [local-name] `(def ~local-name ~local-name))
+              (keys &env))
+       ~form))
 
 (comment
-  (let [a 4] (spy (inc a)))
-  (let [a 4] (macroexpand-1 '(spy (inc a)))))
+  (let [a 1 c 5] (spy (inc a)))
+  (let [^:plop a 2 ^:plip c ^{:a :b :c {:d 37}} {}]
+    (spy (+ a (inc (count c))))))
 
+
+(def dummy-nrepl-middleware
+  "To be injected alongside the rest of the tooling ahead of the code you'd like
+  to probe. This allows the `chpill.sonde/spy` macro to be called from anywhere
+  without having to require it first."
+  identity)
